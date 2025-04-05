34d91386d99cc1e57cb532981308a87996337cd6
Murshi <muchumurshid50@gmail.com>
2025-04-05 02:03:02 +0100

    storing bot messages, sending and receiving messages to and from bot

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,6 +2,11 @@ import cloudinary from "../lib/cloudinary.js";
 import { getReceiverSocketId, io } from "../lib/socket.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
+import axios from "axios";
+
+import dotenv from "dotenv";
+dotenv.config();
+
 
 export const getUsersForSidebar = async (req, res) => {
     // get user id, get user ids other than/ != current user for the side bar 
@@ -36,44 +41,67 @@ export const getMessages = async (req, res) => {
 };
   
 
-export const sendMessage = async (req, res) =>{
-    try {
-        const {text ,image} = req.body;
-        const {id: receiverId} = req.params;
-        const senderId = req.user._id;
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
 
-        let imageUrl;
-        if (image){
-            //upload base64 image to cloudinary
-            const uploadResponse = await cloudinary.uploader.upload(image);
-            imageUrl = uploadResponse.secure_url;
-        }
-
-        //new message and save
-
-        const newMessage = new Message({
-            senderId,
-            receiverId,
-            text,
-            image: imageUrl
-        })
-
-        await newMessage.save()
-
-        res.status(200).json(newMessage)
-
-        //After saving in db, we are sending this realtime to the reciever.
-        // "newMessage" is event name, then data.
-        // This event can listen/subscribe from client to make changes/update in the UI
-
-        const receiverSocketId = getReceiverSocketId(receiverId)
-        if(receiverSocketId){
-          io.to(receiverSocketId).emit("newMessage", newMessage); 
-        }
-
-
-    } catch (error) {
-        console.log("Error in sendMessage controller", error.message)
-        return res.status(500).json({ message: "Internal server error"});
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
     }
-}
+
+    // Store user's message in DB
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    // Send the initial response to the client immediately
+    res.status(200).json(newMessage);
+
+    // After sending the response, avoid sending it again.
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
+    // Check if the message is directed at the bot
+    if (receiverId === process.env.BOT_USER_ID && text) {
+      // Make the API request to Flask
+      const flaskResponse = await axios.post(process.env.FLASK_API_URL, {
+        message: text,
+        user_id: senderId,
+      });
+
+      const botReply = flaskResponse.data.reply;
+
+      // Create a new message for the bot response
+      const botMessage = new Message({
+        senderId: process.env.BOT_USER_ID,
+        receiverId: senderId,
+        text: botReply,
+        is_bot: true,
+      });
+
+      // Save the bot message to DB
+      await botMessage.save();
+
+      // Send the bot's reply in real-time
+      const userSocketId = getReceiverSocketId(senderId);
+      if (userSocketId) {
+        io.to(userSocketId).emit("newMessage", botMessage);
+      }
+    }
+
+  } catch (error) {
+    console.log("Error in sendMessage controller", error.message);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};

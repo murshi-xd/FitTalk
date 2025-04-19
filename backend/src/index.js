ea5c560c5f73417cc07c7d9ebc37001f1f6d8d7f
Murshi <muchumurshid50@gmail.com>
2025-04-20 00:43:28 +0100

    nginx implimented with ssl

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,55 +2,52 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { fileURLToPath } from 'url';
 
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
 
-import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth_route.js";
 import messageRoutes from "./routes/message_route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-
-
-
-// Middleware to parse JSON request bodies
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: 'https://auth.localhost',
+    credentials: true,
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+    ],
+    methods: "GET, POST, PUT, DELETE",
+    preflightContinue: true,
+    optionSuccessStatus:200,
+  })
+);
 
-
-app.use(cors({
-    origin: "https://auth.localhost",
-    credentials:true,
-    allowedHeaders: ["Content-Type", "Authorization"]
-}))
-
-// health check route
-app.get("/", (req, res) => {
-    res.send("API is running");
-});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV ==="production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*",(req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-
 server.listen(PORT, () => {
-    console.log("server is running on PORT:", + PORT);
-    connectDB();
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
-
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
 const API_BASE_URL = import.meta.env.VITE_API_URL || "https://api.auth.localhost";
 
-import axios from 'axios';
-
+import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: `${API_BASE_URL}/api`,
   withCredentials: true,
